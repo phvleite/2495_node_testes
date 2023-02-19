67074a778384f488a516c2da7f2da6627b3d1b3f
Paulo Henrique Vieira Leite <73482438+phvleite@users.noreply.github.com>
2023-02-19 06:55:12 -0300

    feat: inclusas todas as alterções

---
+++ src/tests/routes/editorasRoutes.test.js
@@ -0,0 +1,83 @@
+import request from 'supertest';
+import {
+  describe, expect, it, jest,
+} from '@jest/globals';
+import app from '../../app.js';
+
+let server;
+beforeEach(() => {
+  const PORT = 3000;
+  server = app.listen(PORT);
+});
+
+afterEach(() => {
+  server.close();
+});
+
+describe('GET em /editoras', () => {
+  it('Deve retornar uma lista de editoras', async () => {
+    const resposta = await request(app)
+      .get('/editoras')
+      .set('Accept', 'aplication/json')
+      .expect('content-type', /json/)
+      .expect(200);
+
+    expect(resposta.body[0].email).toEqual('e@e.com');
+  });
+});
+
+let idRespostas;
+describe('POST em editoras', () => {
+  it('Deve adicionar uma nova editora', async () => {
+    const resposta = await request(app)
+      .post('/editoras')
+      .send({
+        nome: 'CDC',
+        cidade: 'São Paulo',
+        email: 's@s.com',
+      })
+      .expect(201);
+
+    idRespostas = resposta.body.content.id;
+  });
+
+  it('Deve não adicionar nada ao passar o body vazio', async () => {
+    await request(app)
+      .post('/editoras')
+      .send({})
+      .expect(400);
+  });
+});
+
+describe('GET em /editoras/id', () => {
+  it('Deve retornar recurso selecionado', async () => {
+    await request(app)
+      .get(`/editoras/${idRespostas}`)
+      .expect(200);
+  });
+});
+
+describe('PUT em /editoras/id', () => {
+  test.each([
+    ['nome', { nome: 'Casa do Código' }],
+    ['cidade', { cidade: 'São Bernado do Campo' }],
+    ['email', { email: 'casadocodigo@cdc.com' }],
+  ])('Deve alterar o campo %s', async (chave, param) => {
+    const requisicao = { request };
+    const spy = jest.spyOn(requisicao, 'request');
+    await requisicao.request(app)
+      .put(`/editoras/${idRespostas}`)
+      .send(param)
+      .expect(204);
+
+    expect(spy).toHaveBeenCalled();
+  });
+});
+
+describe('DELETE em /editoras/id', () => {
+  it('Deletar o recurso adicionado', async () => {
+    await request(app)
+      .delete(`/editoras/${idRespostas}`)
+      .expect(200);
+  });
+});
